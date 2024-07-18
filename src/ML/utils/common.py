1321efc29b47e6cdc413367a78a3da0d7e0bfef2
diyorarti <diyorarti@example.com>
2024-07-18 10:59:38 +0500

    data ingestion

--- src/ML/utils/common.py
+++ src/ML/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.ML.logger import logging
+from ML import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -28,7 +28,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
